07806c78155316a230aeccaba16e69ce3944882c
AhmedPlusPlus <157212041+AhmedMostafa81@users.noreply.github.com>
2026-05-19 21:41:25 +0300

    Update Centroid Decomposition.cpp

--- Graph/Centroid Decomposition.cpp
+++ Graph/Centroid Decomposition.cpp
@@ -85,3 +85,115 @@ void decomposition(int node) {
 }
 
     
+// ------------------------------------------------
+
+// to count all pahts that pass from each node
+
+
+
+//    ->> practice makes perfect
+
+
+#include<bits/stdc++.h>
+using namespace std;
+#define hi cerr<<"Hi\n";
+
+/* ===================  NO CLEAN CODE HERE  =================== */
+
+const int N = 2e5+5;
+vector<int>gr[N];
+string s;
+int a[N] , sz[N];
+long long  ans[N] ;
+bool del[N];
+
+void init_sz(int node , int p) {
+    sz[node] = 0 ;
+    if (del[node])
+        return ;
+    sz[node] = 1;
+    for (auto ch:gr[node]) {
+        if (ch != p) {
+            init_sz(ch , node);
+            sz[node] += sz[ch];
+        }
+    }
+}
+
+int centroid(int node , int p , int tot) {
+    for (auto ch:gr[node])
+        if (ch != p && !del[ch] && sz[ch] > tot / 2 )
+            return centroid(ch , node , tot);
+    return node;
+}
+
+int fr[1 << 20];
+
+void dfs(int node , int p , int mask , int val) {
+    fr[mask] += val;
+    for (auto ch:gr[node])
+        if (!del[ch] && ch != p)
+            dfs(ch , node , mask ^ a[ch] , val);
+}
+
+long long solve(int node , int p , int mask) {
+    long long rt = fr[mask];
+    for (int i = 0 ; i < 20 ; i++)
+        rt += fr[mask ^ (1 << i)];
+    for (auto ch:gr[node])
+        if (ch != p && !del[ch])
+            rt += solve(ch , node , mask ^ a[ch]);
+    ans[node] += rt;
+    return rt;
+}
+
+void decomposition(int node ) {
+    init_sz(node , -1);
+    int cen = centroid(node , -1 , sz[node]);
+    del[cen] = true;
+    long long rt = 0 ;
+    dfs(cen , -1 , a[cen] , 1);
+    rt += fr[0];
+    for (int i = 0; i < 20 ; i++)
+        rt += fr[1 << i];
+    for (auto ch:gr[cen]) {
+        if (del[ch])continue;
+        dfs(ch , cen , a[cen] ^ a[ch] , -1);
+        rt += solve(ch , cen , a[ch]);
+        dfs(ch , cen , a[cen] ^ a[ch] , 1);
+    }
+    dfs(cen , -1 , a[cen] , -1);
+    ans[cen] += rt >> 1;
+    for (auto ch:gr[cen])
+        if (!del[ch])
+            decomposition(ch);
+}
+
+void maybe() {
+
+    int n ;cin >> n ;
+    for (int i  =0 ; i < n - 1; i++) {
+        int x,y;cin >> x >> y;
+        gr[x].push_back(y);
+        gr[y].push_back(x);
+    }
+    cin >> s;
+    for (int i  = 0 ; i < n; i++)
+        a[i + 1] = 1 << (s[i] - 'a');
+    decomposition(1);
+    for (int i =1;i<=n ; i++)
+        cout << ans[i] + 1 << ' ';
+    cout << '\n';
+
+}
+
+
+signed main() {
+
+    ios::sync_with_stdio(0);cin.tie(0);
+    int tc = 1;
+    //cin>>tc;
+    while(tc--)
+        maybe();
+
+}
