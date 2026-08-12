a2dd48dfb528902de245a62b82d27a3064f941be
AhmedPlusPlus <157212041+AhmedMostafa81@users.noreply.github.com>
2026-08-12 15:03:43 +0300

    Update PlusSet.cpp

--- Data Structures/PlusSet.cpp
+++ Data Structures/PlusSet.cpp
@@ -1,85 +1,110 @@
-template <size_t N>
+
+using ull = unsigned long long;
+
 struct PlusSet {
-    static_assert(N>0, "N must be > 0");
-    using u64 = uint64_t;
-    static constexpr size_t B = 64, W = (N + B - 1) / B;
-    u64 a[W];
-    static inline constexpr u64 tail_mask() { return (N % B) ? ((1ULL << (N % B)) - 1) : ~0ULL; }
+    int n;
+    std::vector<ull> a;
 
-    PlusSet(){ reset(); }
-    void reset(){ fill(a, a+W, 0); }
-    void set_all(){ fill(a, a+W, ~0ULL); a[W-1] &= tail_mask(); }
+    Bitset(int n = 0) : n(n), a((n + 63) / 64, 0) {}
 
-    void set(size_t p, bool v = true){ size_t w = p>>6, b = p & 63; if(v) a[w] |= (1ULL<<b); else a[w] &= ~(1ULL<<b); }
-    void flip(size_t p){ a[p>>6] ^= (1ULL << (p & 63)); a[W-1] &= tail_mask(); }
-    void flip_all(){ for(size_t i=0;i<W;++i) a[i] = ~a[i]; a[W-1] &= tail_mask(); }
+    void set(int i)   { a[i / 64] |= (1ULL << (i % 64)); }
+    void reset(int i) { a[i / 64] &= ~(1ULL << (i % 64)); }
+    void flip(int i)  { a[i / 64] ^= (1ULL << (i % 64)); }
+    int test(int i) const { return (a[i / 64] >> (i % 64)) & 1; }
 
-    bool test(size_t p) const { return (a[p>>6] >> (p & 63)) & 1; }
-    bool any() const { for(size_t i=0;i<W;++i) if(a[i]) return true; return false; }
-    bool none() const { return !any(); }
-    size_t count() const { size_t s=0; for(size_t i=0;i<W;++i) s += __builtin_popcountll(a[i]); return s; }
+    // Cleans garbage bits in the last block (crucial for count, ==, and shifts)
+    void clean() {
+        if (n % 64 != 0) a.back() &= (1ULL << (n % 64)) - 1;
+    }
 
-    PlusSet& operator&=(const PlusSet& o){ for(size_t i=0;i<W;++i) a[i] &= o.a[i]; return *this; }
-    PlusSet& operator|=(const PlusSet& o){ for(size_t i=0;i<W;++i) a[i] |= o.a[i]; return *this; }
-    PlusSet& operator^=(const PlusSet& o){ for(size_t i=0;i<W;++i) a[i] ^= o.a[i]; return *this; }
-    PlusSet operator~() const { PlusSet r = *this; for(size_t i=0;i<W;++i) r.a[i] = ~r.a[i]; r.a[W-1] &= tail_mask(); return r; }
-    friend PlusSet operator&(PlusSet x,const PlusSet& y){ x &= y; return x; }
-    friend PlusSet operator|(PlusSet x,const PlusSet& y){ x |= y; return x; }
-    friend PlusSet operator^(PlusSet x,const PlusSet& y){ x ^= y; return x; }
-
-    PlusSet& operator<<=(size_t s){
-        if(s >= N){ reset(); return *this; }
-        size_t big = s >> 6; unsigned rem = s & 63;
-        if(rem == 0){
-            for(size_t i = W; i-- > 0;) a[i] = (i >= big ? a[i-big] : 0);
-        } else {
-            for(size_t i = W; i-- > 0;){
-                u64 v = 0;
-                if(i >= big){
-                    v = a[i-big] << rem;
-                    if(i - big > 0) v |= (a[i-big-1] >> (64 - rem));
-                }
-                a[i] = v;
-            }
-        }
-        a[W-1] &= tail_mask();
+    // ==========================================
+    Bitset& operator|=(const Bitset& o) {
+        for (size_t i = 0; i < a.size(); i++) a[i] |= o.a[i];
+        return *this;
+    }
+    
+    Bitset& operator&=(const Bitset& o) {
+        for (size_t i = 0; i < a.size(); i++) a[i] &= o.a[i];
+        return *this;
+    }
+    
+    Bitset& operator^=(const Bitset& o) {
+        for (size_t i = 0; i < a.size(); i++) a[i] ^= o.a[i];
         return *this;
     }
 
-    PlusSet& operator>>=(size_t s){
-        if(s >= N){ reset(); return *this; }
-        size_t big = s >> 6; unsigned rem = s & 63;
-        if(rem == 0){
-            for(size_t i=0;i<W;++i) a[i] = (i + big < W ? a[i + big] : 0);
-        } else {
-            for(size_t i=0;i<W;++i){
-                u64 v = 0;
-                if(i + big < W){
-                    v = a[i + big] >> rem;
-                    if(i + big + 1 < W) v |= (a[i + big + 1] << (64 - rem));
-                }
-                a[i] = v;
+    // ==========================================
+    Bitset& operator<<=(int k) {
+        if (k >= n) { clear(); return *this; }
+        int blocks = k / 64, bits = k % 64;
+        
+        // Go backwards to avoid overwriting data we still need
+        for (int i = (int)a.size() - 1; i >= blocks; i--) {
+            a[i] = a[i - blocks] << bits;
+            if (bits > 0 && i - blocks - 1 >= 0) {
+                a[i] |= (a[i - blocks - 1] >> (64 - bits));
             }
         }
+        for (int i = 0; i < blocks; i++) a[i] = 0; // zero out lower blocks
+        clean();
         return *this;
     }
 
-    PlusSet operator<<(size_t k) const { PlusSet r = *this; r <<= k; return r; }
-    PlusSet operator>>(size_t k) const { PlusSet r = *this; r >>= k; return r; }
+    Bitset& operator>>=(int k) {
+        if (k >= n) { clear(); return *this; }
+        int blocks = k / 64, bits = k % 64;
+        
+        // Go forwards
+        for (int i = 0; i + blocks < (int)a.size(); i++) {
+            a[i] = a[i + blocks] >> bits;
+            if (bits > 0 && i + blocks + 1 < (int)a.size()) {
+                a[i] |= (a[i + blocks + 1] << (64 - bits));
+            }
+        }
+        for (int i = (int)a.size() - blocks; i < (int)a.size(); i++) a[i] = 0;
+        clean();
+        return *this;
+    }
 
+    // ==========================================
+    void clear() {
+        std::fill(a.begin(), a.end(), 0ULL);
+    }
+
+    int count() const {
+        int cnt = 0;
+        for (ull x : a) cnt += __builtin_popcountll(x);
+        return cnt;
+    }
+
+    bool operator==(const Bitset& o) const {
+        for (size_t i = 0; i < a.size(); i++) {
+            if (a[i] != o.a[i]) return false;
+        }
+        return true;
+    }
+
+    // Fast iteration: Use like `for(int i = b.find_first(); i != -1; i = b.find_next(i))`
     int find_first() const {
-        for(size_t i=0;i<W;++i){
-            u64 w = a[i];
-            if(w){ int t = __builtin_ctzll(w); int pos = int(i*64 + t); return pos >= int(N) ? -1 : pos; }
+        for (size_t i = 0; i < a.size(); i++) {
+            if (a[i]) return i * 64 + __builtin_ctzll(a[i]);
         }
         return -1;
     }
-    int find_next(int pos) const {
-        if(pos + 1 >= int(N)) return -1;
-        size_t i = (pos + 1) >> 6; unsigned b = (pos + 1) & 63;
-        u64 w = a[i] & (~0ULL << b);
-        if(w) return int(i*64 + __builtin_ctzll(w));
-        for(size_t j = i+1; j < W; ++j) if(a[j]) return int(j*64 + __builtin_ctzll(a[j]));
+
+    int find_next(int i) const {
+        i++;
+        if (i >= n) return -1;
+        int b = i / 64, bit = i % 64;
+        
+        // Check remainder of the current block
+        ull masked = a[b] & (~0ULL << bit);
+        if (masked) return b * 64 + __builtin_ctzll(masked);
+        
+        // Check subsequent blocks
+        for (size_t j = b + 1; j < a.size(); j++) {
+            if (a[j]) return j * 64 + __builtin_ctzll(a[j]);
+        }
         return -1;
     }
 };
