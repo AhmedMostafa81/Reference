cb92bdf5893aa0ae4d87c5c46d77bfbf484d1116
AhmedPlusPlus <157212041+AhmedMostafa81@users.noreply.github.com>
2026-08-13 18:15:52 +0300

    Refactor ordered_set and add new templates
    Refactor ordered_set definition and add templates for ordered_multiset and ordered_map.

--- Data Structures/Orderd Set.cpp
+++ Data Structures/Orderd Set.cpp
@@ -2,7 +2,9 @@
 #include <ext/pb_ds/assoc_container.hpp>
 #include <ext/pb_ds/tree_policy.hpp>
 using namespace __gnu_pbds;
-#define ordered_set tree<int, null_type,less_equal<int>, rb_tree_tag,tree_order_statistics_node_update>
+template<class T> using ordered_set = tree<T, null_type, std::less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+template<class T> using ordered_multiset = tree<T, null_type, std::less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;
+template <typename T, typename R> using ordered_map = tree<T, R, std::less<T>, rb_tree_tag, tree_order_statistics_node_update>;
 
 ordered_set st;
 st.order_of_key(number);// number of elem. less than (..)
